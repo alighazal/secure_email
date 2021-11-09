1cda3ac51350e5576890bf2234670db877f8099f
mayshalaby <mayshalaby@aucegypt.edu>
2021-11-09 13:52:08 +0200

    verify_msg_integrity

--- receive.py
+++ receive.py
@@ -13,11 +13,20 @@ def decrypt_message_key_with_RSA(encrypted_key, recipient_private_key):
 
 def decrypt_message_with_AES(encrypted_message, decrypted_symmetric_key):
 
+def verfiy_message_integrity(decrypted_message, message_hash):
+    #hash the message
+    #compare hashes
+    #return true if match
+    #else, return false
+
 def decrypt_message(sender_email, recipient_email, message_signature):
     sender_public_key = lookup_public_key_by_email(sender_email)
     (encrypted_message, encrypted_key) = decrypt_signature(sender_public_key, message_signature)
     #get the private key from ClientUI<-------------------------------------------------------------------------------------
     decrypted_symmetric_key = decrypt_message_key_with_RSA(encrypted_key, recipient_private_key)
-    decrypted_message = decrypt_message_with_AES(encrypted_message, decrypted_symmetric_key)
-    return (decrypted_message)
+    (decrypted_message, message_hash) = decrypt_message_with_AES(encrypted_message, decrypted_symmetric_key)
+    if(verfiy_message_integrity(decrypted_message, message_hash)):
+        return (decrypted_message)
+    else
+        print("message corrupted")    
    
